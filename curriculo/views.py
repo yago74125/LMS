025d631921660aed8db0278a1827edc24ac26a6d
Yago Paneque Marques <1802039@SALAS.AULAS>
2019-05-13 11:32:26 -0300

    aula

--- curriculo/views.py
+++ curriculo/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from curriculo.models import Curso
 
 def curso(request, sigla):
+    c = get_object_or_404(Curso, sigla=sigla)
     context = {
-        'curso': get_object_or_404(Curso, sigla=sigla)
+        'curso': c,
+        'matriz': c.monta_matriz()
     }
     return render(request, 'curriculo/curso.html', context)
 
