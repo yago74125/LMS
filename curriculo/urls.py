95b0d004a86a6af981e47cccfae3f5ce963c3ebd
luccasr73 <luccasrobert@hotmail.com>
2019-05-22 08:36:54 -0300

    Fix broken links and add some initial data

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'curriculo'
 urlpatterns = [
-    path('<str:sigla>/', views.curso, name='curso'), 
-    path('', views.cursos, name='cursos')
+    path('<str:sigla>/', views.curso, name='curso'),
+    path('disciplinas/<int:id_disciplina>',views.disciplina, name='disciplina')
 ]
--- curriculo/views.py
+++ curriculo/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 
-from curriculo.models import Curso
+from curriculo.models import Curso,Disciplina
 
 def curso(request, sigla):
-    c = get_object_or_404(Curso, sigla=sigla)
     context = {
-        'curso': c,
-        'matriz': c.monta_matriz()
+        'curso': get_object_or_404(Curso, sigla=sigla)
     }
     return render(request, 'curriculo/curso.html', context)
 
-def cursos(request):
+def disciplina(request, id_disciplina):
     context = {
-        'cursos': Curso.objects.all()
-    } 
-    return render(request, 'curriculo/cursos.html', context)   
+        'disciplina': get_object_or_404(Disciplina, id=id_disciplina)
+    }
+    return render(request, 'curriculo/disciplina.html', context)
